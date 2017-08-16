dde79e68870bc0a590ae791c551b5fb97b7a4e64
dimMaryanyanto93 <engineer.dimmaryanto93@outlook.com>
2017-08-17 02:16:24 +0700

    udpate tipe data number

--- data-types/dataType.ts
+++ data-types/dataType.ts
@@ -2,3 +2,10 @@
 
 let isDone: boolean = false
 console.info("Apakah anda yakin ? : " + isDone)
+
+// tipe data number
+
+let decimal: number = 6.10;
+let hexadecimal: number = 0xf00d;
+let decimal_minus: number = -10;
+console.info("Number yang diterima: [" + decimal + ", " + hexadecimal + ", " + decimal_minus + "]");
